5c97ec8bdde07861035331f8950ba06f0c80ec25
iamkroot <kroot.patel@gmail.com>
2021-11-17 00:18:41 +0530

    Hold to activate numlock

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ fn toggle_numlock(numlock: &mut bool, touchpad_dev: &mut Device) {
 }
 
 fn main() {
+    // TODO: Use i2cdev crate- wait for `force_new` release
     let (_, touchpad_ev_id, i2c) = read_proc_input().expect("Couldn't get proc input devices");
     let mut touchpad_dev = open_input_evdev(touchpad_ev_id);
     let (minx, maxx) = get_minmax(&touchpad_dev, EV_ABS::ABS_X);
@@ -71,6 +72,11 @@ fn main() {
     let mut numlock: bool = false;
     let mut cur_key: Option<EV_KEY> = None;
     let mut finger_state = FingerState::Lifted;
+    let mut tap_started_at = i64::MAX;
+    let mut tapped_outside_numlock_bbox: bool = false;
+    /// 1sec
+    const HOLD_DURATION: i64 = 1; // TODO: Allow subsecond durations, will have to implement Ops::Sub
+
     // TODO: Read I2C brightness while starting up to see if numlock is enabled
     loop {
         let ev = touchpad_dev
@@ -79,6 +85,9 @@ fn main() {
         if let Ok(ev) = ev {
             match ev.event_code {
                 evdev_rs::enums::EventCode::EV_ABS(evdev_rs::enums::EV_ABS::ABS_MT_POSITION_X) => {
+                    // what happens when it goes outside bbox of cur_key while dragging?
+                    // should we move to new key?
+                    // TODO: Check official Windows driver behaviour
                     pos_x = ev.value as f32;
                     continue;
                 }
@@ -90,6 +99,11 @@ fn main() {
                     if ev.value == 0 {
                         // end of tap
                         finger_state = FingerState::Lifted;
+                        if !tapped_outside_numlock_bbox
+                            && ev.time.tv_sec - tap_started_at >= HOLD_DURATION
+                        {
+                            toggle_numlock(&mut numlock, &mut touchpad_dev);
+                        }
                         if let Some(key) = cur_key {
                             if layout.needs_multikey(key) {
                                 kb.multi_keyup(&layout.multikeys(key));
@@ -102,12 +116,14 @@ fn main() {
                         if finger_state == FingerState::Lifted {
                             // start of tap
                             finger_state = FingerState::Touching;
+                            tap_started_at = ev.time.tv_sec;
+                            tapped_outside_numlock_bbox = false;
                         }
                         // TODO: Support calc key (top left)
-                        // TODO: Hold to toggle numlock, instead of tap
                         if layout.in_numpad_bbox(pos_x, pos_y) {
-                            finger_state = FingerState::Lifted;
-                            toggle_numlock(&mut numlock, &mut touchpad_dev);
+                            finger_state = FingerState::Tapping;
+                        } else {
+                            tapped_outside_numlock_bbox = true;
                         }
                     }
                 }
