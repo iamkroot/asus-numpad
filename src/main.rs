ca4e62e12cab86461264f629d0ae05076c1b0037
iamkroot <kroot.patel@gmail.com>
2021-11-28 09:48:11 +0530

    Move main logic to Numpad struct

--- src/main.rs
+++ src/main.rs
@@ -44,18 +44,6 @@ fn activate_numlock() {
         .expect("Numlock");
 }
 
-fn toggle_numlock(numlock: &mut bool, touchpad_dev: &mut Device) {
-    *numlock = if *numlock {
-        deactivate_numlock();
-        touchpad_dev.grab(GrabMode::Ungrab).expect("UNGRAB");
-        false
-    } else {
-        activate_numlock();
-        touchpad_dev.grab(GrabMode::Grab).expect("GRAB");
-        true
-    };
-}
-
 trait ElapsedSince {
     /// Calculate time elapsed since `other`.
     ///
@@ -85,107 +73,179 @@ impl ElapsedSince for TimeVal {
     }
 }
 
-fn main() {
-    // TODO: Use i2cdev crate- wait for `force_new` release
-    let (_, touchpad_ev_id, i2c) = read_proc_input().expect("Couldn't get proc input devices");
-    let mut touchpad_dev = open_input_evdev(touchpad_ev_id);
-    let (minx, maxx) = get_minmax(&touchpad_dev, EV_ABS::ABS_X);
-    let (miny, maxy) = get_minmax(&touchpad_dev, EV_ABS::ABS_Y);
-    let layout = NumpadLayout::m433ia(minx, maxx, miny, maxy);
-    let kb = DummyKeyboard::new(&layout);
+#[derive(Debug, Clone, Copy)]
+struct TouchpadState {
+    posx: f32,
+    posy: f32,
+    finger_state: FingerState,
+    numlock: bool,
+    cur_key: Option<EV_KEY>,
+    tap_started_at: TimeVal,
+    tapped_outside_numlock_bbox: bool,
+}
 
-    let mut pos_x = 0.0;
-    let mut pos_y = 0.0;
-    let mut numlock: bool = false;
-    let mut cur_key: Option<EV_KEY> = None;
-    let mut finger_state = FingerState::Lifted;
-    let mut tap_started_at = TimeVal {
-        tv_sec: 0,
-        tv_usec: 0,
-    };
-    let mut tapped_outside_numlock_bbox: bool = false;
-    /// 1sec
-    const HOLD_DURATION: TimeVal = TimeVal {
-        tv_sec: 0,
-        tv_usec: 750_000,
-    };
-    // TODO: Read I2C brightness while starting up to see if numlock is enabled
-    loop {
-        let ev = touchpad_dev
-            .next_event(ReadFlag::NORMAL | ReadFlag::BLOCKING)
-            .map(|val| val.1);
-        if let Ok(ev) = ev {
-            match ev.event_code {
-                EventCode::EV_ABS(EV_ABS::ABS_MT_POSITION_X) => {
-                    // what happens when it goes outside bbox of cur_key while dragging?
-                    // should we move to new key?
-                    // TODO: Check official Windows driver behaviour
-                    pos_x = ev.value as f32;
-                    continue;
-                }
-                EventCode::EV_ABS(EV_ABS::ABS_MT_POSITION_Y) => {
-                    pos_y = ev.value as f32;
-                    continue;
-                }
-                EventCode::EV_KEY(EV_KEY::BTN_TOOL_FINGER) => {
-                    if ev.value == 0 {
-                        // end of tap
-                        finger_state = FingerState::Lifted;
-                        if let Some(key) = cur_key {
-                            if layout.needs_multikey(key) {
-                                kb.multi_keyup(&layout.multikeys(key));
+impl TouchpadState {
+    fn toggle_numlock(&mut self) -> bool {
+        self.numlock = !self.numlock;
+        self.numlock
+    }
+}
+
+impl Default for TouchpadState {
+    fn default() -> Self {
+        Self {
+            posx: 0.0,
+            posy: 0.0,
+            finger_state: FingerState::Lifted,
+            numlock: false,
+            cur_key: None,
+            tap_started_at: TimeVal {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            tapped_outside_numlock_bbox: false,
+        }
+    }
+}
+
+struct Numpad {
+    evdev: Device,
+    dummy_kb: DummyKeyboard,
+    layout: NumpadLayout,
+    state: TouchpadState,
+}
+
+impl std::fmt::Debug for Numpad {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Numpad")
+            .field("evdev", &self.evdev.file())
+            .field("keyboard", &self.dummy_kb)
+            .field("state", &self.state)
+            .field("layout", &self.layout)
+            .finish()
+    }
+}
+
+impl Numpad {
+    fn new(evdev: Device, dummy_kb: DummyKeyboard, layout: NumpadLayout) -> Self {
+        Self {
+            evdev,
+            dummy_kb,
+            layout,
+            state: TouchpadState::default(),
+        }
+    }
+
+    fn toggle_numlock(&mut self) {
+        if self.state.toggle_numlock() {
+            activate_numlock();
+            self.evdev.grab(GrabMode::Grab).expect("GRAB");
+        } else {
+            deactivate_numlock();
+            self.evdev.grab(GrabMode::Ungrab).expect("UNGRAB");
+        }
+    }
+
+    fn process(&mut self) {
+        // TODO: Make this user configurable
+        const HOLD_DURATION: TimeVal = TimeVal {
+            tv_sec: 0,
+            tv_usec: 750_000,
+        };
+        loop {
+            let ev = self
+                .evdev
+                .next_event(ReadFlag::NORMAL | ReadFlag::BLOCKING)
+                .map(|val| val.1);
+            if let Ok(ev) = ev {
+                match ev.event_code {
+                    EventCode::EV_ABS(EV_ABS::ABS_MT_POSITION_X) => {
+                        // what happens when it goes outside bbox of cur_key while dragging?
+                        // should we move to new key?
+                        // TODO: Check official Windows driver behaviour
+                        self.state.posx = ev.value as f32;
+                        continue;
+                    }
+                    EventCode::EV_ABS(EV_ABS::ABS_MT_POSITION_Y) => {
+                        self.state.posy = ev.value as f32;
+                        continue;
+                    }
+                    EventCode::EV_KEY(EV_KEY::BTN_TOOL_FINGER) => {
+                        if ev.value == 0 {
+                            // end of tap
+                            self.state.finger_state = FingerState::Lifted;
+                            if let Some(key) = self.state.cur_key {
+                                if self.layout.needs_multikey(key) {
+                                    self.dummy_kb.multi_keyup(&self.layout.multikeys(key));
+                                } else {
+                                    self.dummy_kb.keyup(key);
+                                }
+                                self.state.cur_key = None;
+                            }
+                        } else if ev.value == 1 {
+                            if self.state.finger_state == FingerState::Lifted {
+                                // start of tap
+                                self.state.finger_state = FingerState::Touching;
+                                self.state.tap_started_at = ev.time;
+                                self.state.tapped_outside_numlock_bbox = false;
+                            }
+                            // TODO: Support calc key (top left)
+                            if self.layout.in_numpad_bbox(self.state.posx, self.state.posy) {
+                                self.state.finger_state = FingerState::Tapping;
                             } else {
-                                kb.keyup(key);
+                                self.state.tapped_outside_numlock_bbox = true
                             }
-                            cur_key = None;
-                        }
-                    } else if ev.value == 1 {
-                        if finger_state == FingerState::Lifted {
-                            // start of tap
-                            finger_state = FingerState::Touching;
-                            tap_started_at = ev.time;
-                            tapped_outside_numlock_bbox = false;
-                        }
-                        // TODO: Support calc key (top left)
-                        if layout.in_numpad_bbox(pos_x, pos_y) {
-                            finger_state = FingerState::Tapping;
-                        } else {
-                            tapped_outside_numlock_bbox = true
                         }
                     }
-                }
-                EventCode::EV_MSC(EV_MSC::MSC_TIMESTAMP) => {
-                    // The toggle should happen automatically after HOLD_DURATION, even if user is
-                    // still touching the numpad bbox.
-                    if finger_state == FingerState::Tapping && !tapped_outside_numlock_bbox {
-                        if layout.in_numpad_bbox(pos_x, pos_y) {
-                            if ev.time.elapsed_since(tap_started_at) >= HOLD_DURATION {
-                                toggle_numlock(&mut numlock, &mut touchpad_dev);
-                                // If user doesn't lift the finger quickly, we don't want to keep
-                                // toggling, so assume finger was moved. 
-                                // Can't do finger_state = Lifted, since that would start another tap
-                                // Can't do finger_state = Touching, since that would cause numpad
-                                // keypresses (we don't check for margins in layout.get_key yet)
-                                tapped_outside_numlock_bbox = true;
+                    EventCode::EV_MSC(EV_MSC::MSC_TIMESTAMP) => {
+                        // The toggle should happen automatically after HOLD_DURATION, even if user is
+                        // still touching the numpad bbox.
+                        if self.state.finger_state == FingerState::Tapping
+                            && !self.state.tapped_outside_numlock_bbox
+                        {
+                            if self.layout.in_numpad_bbox(self.state.posx, self.state.posy) {
+                                if ev.time.elapsed_since(self.state.tap_started_at) >= HOLD_DURATION
+                                {
+                                    self.toggle_numlock();
+                                    // If user doesn't lift the finger quickly, we don't want to keep
+                                    // toggling, so assume finger was moved.
+                                    // Can't do finger_state = Lifted, since that would start another tap
+                                    // Can't do finger_state = Touching, since that would cause numpad
+                                    // keypresses (we don't check for margins in layout.get_key yet)
+                                    self.state.tapped_outside_numlock_bbox = true;
+                                }
+                            } else {
+                                self.state.tapped_outside_numlock_bbox = true;
                             }
-                        } else {
-                            tapped_outside_numlock_bbox = true;
                         }
                     }
+                    _ => (),
                 }
-                _ => (),
-            }
-            if numlock && finger_state == FingerState::Touching {
-                cur_key = layout.get_key(pos_x, pos_y);
-                if let Some(key) = cur_key {
-                    finger_state = FingerState::Tapping;
-                    if layout.needs_multikey(key) {
-                        kb.multi_keydown(&layout.multikeys(key));
-                    } else {
-                        kb.keydown(key);
+                if self.state.numlock && self.state.finger_state == FingerState::Touching {
+                    self.state.cur_key = self.layout.get_key(self.state.posx, self.state.posy);
+                    if let Some(key) = self.state.cur_key {
+                        self.state.finger_state = FingerState::Tapping;
+                        if self.layout.needs_multikey(key) {
+                            self.dummy_kb.multi_keydown(&self.layout.multikeys(key));
+                        } else {
+                            self.dummy_kb.keydown(key);
+                        }
                     }
                 }
             }
         }
     }
+}
+
+fn main() {
+    // TODO: Use i2cdev crate
+    let (_, touchpad_ev_id, _) = read_proc_input().expect("Couldn't get proc input devices");
+    let touchpad_dev = open_input_evdev(touchpad_ev_id);
+    let (minx, maxx) = get_minmax(&touchpad_dev, EV_ABS::ABS_X);
+    let (miny, maxy) = get_minmax(&touchpad_dev, EV_ABS::ABS_Y);
+    let layout = NumpadLayout::m433ia(minx, maxx, miny, maxy);
+    let kb = DummyKeyboard::new(&layout);
+
+    let mut numpad = Numpad::new(touchpad_dev, kb, layout);
+    numpad.process();
 }
