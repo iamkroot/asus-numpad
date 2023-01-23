32858c44a3bf29f5253bc95a29e7d20a393d165a
Imran Khan <encrustace@gmail.com>
2023-01-24 01:47:29 +0530

    install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Checking if the script is runned as root (via sudo or other)
+if [[ $(id -u) != 0 ]];
+then
+    echo "Please run the installation script as root (using sudo for example)"
+    exit 1
+fi
+# Checking if config file already exists
+if [ -f "/etc/xdg/asus_numpad.toml" ];
+then
+    echo "Config file already exists!"
+    cat /etc/xdg/asus_numpad.toml
+else
+    echo "Creating config file"
+    touch /etc/xdg/asus_numpad.toml
+    echo 'layout = "M433IA"' > /etc/xdg/asus_numpad.toml
+fi
+
+# Copying program file
+if [ -f "$PWD/target/debug/asus-numpad" ];
+then
+    echo "Copying program to /etc/bin/"
+    cp -f "$PWD/target/debug/asus-numpad" /etc/bin/asus-numpad
+else
+    echo "Program does not exist..."
+    exit 1
+fi
+# Copying systemctl services
+cp -f "$PWD/tools/asus-numpad.service" /etc/systemd/system/
+# Enabling and starting systemctl service
+systemctl enable asus-numpad.service
+systemctl restart asus-numpad.service
+
